3ee2e6a980cf2d44f162ebdb89f7a0b1caa376ed
raphsys <42214513+raphsys@users.noreply.github.com>
2025-05-10 12:30:26 +0000

    Add files via upload
    Signed-off-by: raphsys <42214513+raphsys@users.noreply.github.com>

--- python/setup.py
+++ python/setup.py
@@ -1,25 +1,42 @@
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 import pybind11
+import os
+
+cpp_src = "../cpp/src/"
+source_files = [os.path.join(cpp_src, f) for f in [
+    "python_bindings.cpp",
+    "linear.cpp",
+    "conv2d.cpp",
+    "activation.cpp",
+    "loss.cpp",
+    "optimizer.cpp",
+    "data_loader.cpp",
+    "autograd.cpp",
+    "nncell.cpp",
+    "tensor.cpp",
+    "napcas.cpp",
+]]
 
 ext_modules = [
     Extension(
-        'napcas',
-        sources=['../cpp/src/python_bindings.cpp', '../cpp/src/activation.cpp', '../cpp/src/autograd.cpp', '../cpp/src/conv2d.cpp', '../cpp/src/data_loader.cpp', '../cpp/src/linear.cpp', '../cpp/src/loss.cpp', '../cpp/src/napcas.cpp', '../cpp/src/nncell.cpp', '../cpp/src/optimizer.cpp', '../cpp/src/tensor.cpp'],
+        "_napcas",
+        source_files,
         include_dirs=[
+            "../cpp/include",
             pybind11.get_include(),
-            pybind11.get_include(user=True),
-            '../cpp/include'
         ],
-        library_dirs=['../cpp/build'],
-        libraries=['napcas'],
-        language='c++',
-        extra_compile_args=['-std=c++17'],
-    )
+        language="c++",
+        extra_compile_args=["-std=c++17"],
+    ),
 ]
 
 setup(
-    name='napcas',
-    version='0.1',
+    name="napcas",
+    version="0.1.0",
+    packages=["napcas"],
     ext_modules=ext_modules,
-    install_requires=['pybind11>=2.6'],
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
 )
+
